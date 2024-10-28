3e5277a36c62ecba46e38c7dee81ca16cecf249b
darqus <voladores@inbox.ru>
2024-10-28 19:00:21 +0300

    add page

---
+++ page.js
@@ -0,0 +1,17 @@
+chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+  addImagesToContainer(message)
+  sendResponse('OK')
+})
+
+/**
+ * Функция, которая будет генерировать HTML-разметку
+ * списка изображений
+ * @param {} urls - Массив путей к изображениям
+ */
+const addImagesToContainer = (urls) => {
+  // TODO Создать HTML-разметку в элементе <div> с
+  // классом container для показа
+  // списка изображений и выбора изображений,
+  // для выгрузки в ZIP-архив
+  document.write(JSON.stringify(urls))
+}
