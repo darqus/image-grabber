2f811dbceb7adb336fa32e2f259f20846f928247
darqus <voladores@inbox.ru>
2024-10-28 19:02:22 +0300

    ref to arrow

--- page.js
+++ page.js
@@ -1,4 +1,4 @@
-chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   addImagesToContainer(message)
   sendResponse('OK')
 })
