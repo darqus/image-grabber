b564dd9c9f2cb90740b4779bc90a6421a7861cab
darqus <voladores@inbox.ru>
2024-10-29 00:16:46 +0300

    ref page

--- page.js
+++ page.js
@@ -3,75 +3,94 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   sendResponse('OK')
 })
 
-const addImagesToContainer = (imageUrls) => {
-  if (!imageUrls || !imageUrls.length) {
-    return
-  }
+const addImagesToContainer = (urls) => {
+  if (!urls || !urls.length) return
 
   const container = document.querySelector('.container')
-  imageUrls.forEach((url) => addImageNode(container, url))
+  urls.forEach((url) => addImageNode(container, url))
 }
 
 const addImageNode = (container, url) => {
-  const imageDiv = document.createElement('div')
-  imageDiv.className = 'imageDiv'
+  const div = document.createElement('div')
+  div.className = 'imageDiv'
   const img = document.createElement('img')
   img.src = url
-  imageDiv.appendChild(img)
+  div.appendChild(img)
   const checkbox = document.createElement('input')
   checkbox.type = 'checkbox'
-  checkbox.setAttribute('data-url', url)
-  imageDiv.appendChild(checkbox)
-  container.appendChild(imageDiv)
+  checkbox.setAttribute('url', url)
+  div.appendChild(checkbox)
+  container.appendChild(div)
 }
 
 document.getElementById('selectAll').addEventListener('change', (event) => {
-  const checkboxes = document.querySelectorAll('.container input')
-  for (const checkbox of checkboxes) {
-    checkbox.checked = event.target.checked
+  const items = document.querySelectorAll('.container input')
+  for (const item of items) {
+    item.checked = event.target.checked
   }
 })
 
 document.getElementById('downloadBtn').addEventListener('click', async () => {
   try {
-    const selectedUrls = getSelectedImageUrls()
-    const archive = await createArchive(selectedUrls)
+    const urls = getSelectedUrls()
+    const archive = await createArchive(urls)
     downloadArchive(archive)
   } catch (error) {
     alert(error.message)
   }
 })
 
-const getSelectedImageUrls = () => {
-  const checkedCheckboxes = document.querySelectorAll(
-    '.container input[type="checkbox"]:checked',
-  )
-  const selectedUrls = Array.from(checkedCheckboxes).map((checkbox) =>
-    checkbox.getAttribute('data-url'),
-  )
-  if (!selectedUrls.length) {
-    throw new Error('Please select at least one image')
+const getSelectedUrls = () => {
+  const urls = Array.from(document.querySelectorAll('.container input'))
+    .filter((item) => item.checked)
+    .map((item) => item.getAttribute('url'))
+
+  if (!urls || !urls.length) {
+    throw new Error('Please, select at least one image')
   }
-  return selectedUrls
+
+  return urls
 }
 
 const createArchive = async (urls) => {
   const zip = new JSZip()
-  for (const url of urls) {
-    const response = await fetch(url)
-    const blob = await response.blob()
-    const filename = url.split('/').pop()
-    zip.file(filename, blob)
+
+  for (const index in urls) {
+    try {
+      const url = urls[index]
+      const response = await fetch(url)
+      const blob = await response.blob()
+      zip.file(getFileName(index, blob), blob)
+    } catch (error) {
+      console.error(error)
+    }
   }
-  return zip.generateAsync({ type: 'blob' })
+
+  return zip.generateAsync({
+    type: 'blob',
+    compression: 'DEFLATE',
+    compressionOptions: {
+      level: 9,
+    },
+  })
+}
+
+const getFileName = (index, blob) => {
+  const name = parseInt(index) + 1
+  const [type, extension] = blob.type.split('/')
+  if (type !== 'image' || blob.size <= 0) {
+    throw new Error('Incorrect content')
+  }
+
+  return `${name}.${extension.split('+').shift()}`
 }
 
 const downloadArchive = (archive) => {
-  const a = document.createElement('a')
-  a.href = URL.createObjectURL(archive)
-  a.download = 'images.zip'
-  document.body.appendChild(a)
-  a.click()
-  document.body.removeChild(a)
-  URL.revokeObjectURL(a.href)
+  const link = document.createElement('a')
+  link.href = URL.createObjectURL(archive)
+  link.download = 'images.zip'
+  document.body.appendChild(link)
+  link.click()
+  URL.revokeObjectURL(link.href)
+  document.body.removeChild(link)
 }
