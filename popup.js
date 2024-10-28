153c4e8af5592892e2f35a750564d79dce06a528
darqus <voladores@inbox.ru>
2024-10-29 00:23:38 +0300

    ref popup

--- popup.js
+++ popup.js
@@ -1,46 +1,35 @@
-const grabBtn = document.getElementById('grabBtn')
-grabBtn.addEventListener('click', () => {
-  // Get active browser tab
-  chrome.tabs.query({ active: true }, (tabs) => {
-    const tab = tabs[0]
-    if (tab) {
-      execScript(tab)
-    } else {
-      alert('There are no active tabs')
-    }
-  })
+document.getElementById('grabBtn').addEventListener('click', async () => {
+  const tabs = await chrome.tabs.query({ active: true })
+  const activeTab = tabs[0]
+
+  if (!activeTab) {
+    alert('There are no active tabs')
+    return
+  }
+
+  executeScript(activeTab)
 })
 
-const execScript = (tab) => {
-  // Execute a function on a page of the current browser tab
-  // and process the result of execution
+const executeScript = (tab) => {
   chrome.scripting.executeScript(
     {
       target: { tabId: tab.id, allFrames: true },
-      func: grabImages,
+      func: extractImageUrls,
     },
-    onResult,
+    handleResult,
   )
 }
 
-const grabImages = () => {
+const extractImageUrls = () => {
   const images = document.querySelectorAll('img')
   return Array.from(images).map((image) => image.src)
 }
 
-const onResult = (frames) => {
-  // If script execution failed on remote end
-  // and could not return results
-  if (!frames || !frames.length) {
-    alert('Could not retrieve images from specified page')
-    return
-  }
-  // Combine arrays of image URLs from
-  // each frame to a single array
-  const imageUrls = frames
-    .map((frame) => frame.result)
-    .reduce((r1, r2) => r1.concat(r2))
-  // Open a page with a list of images and send urls to it
+const handleResult = (result) => {
+  const imageUrls = result
+    .map((frameResult) => frameResult.result)
+    .reduce((urls, frameUrls) => urls.concat(frameUrls), [])
+
   openImagesPage(imageUrls)
 }
 
